c1d42570999fe3fe16fb11ee3fa7ac72b7f1a1f1
renknotts <rknotts@byui.edu>
2022-01-21 20:10:26 -0700

    commit

---
+++ numbers.js
@@ -0,0 +1,13 @@
+document.querySelector('#copyright').textContent = new Date().getFullYear();
+
+function getTotal() {
+	// INPUT
+	 let q1 = 0;
+	 let p1 = 0; 
+	
+	// PROCESSING
+	 
+	
+	//OUTPUT
+	 
+}
